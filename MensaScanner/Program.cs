35d5c420e9ea73de35ea91fd9482e262e1d8af85
Jan Wichelmann <jan.wichelmann@hotmail.de>
2018-10-18 01:04:07 +0200

    WebEx Teams support

--- MensaScanner/Program.cs
+++ MensaScanner/Program.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml.Linq;
@@ -26,28 +29,123 @@ namespace MensaScanner
         static string UkshUrl = @"https://www.uksh.de/uksh_media/Speisepl%C3%A4ne/L%C3%BCbeck+_+UKSH_Bistro/Speiseplan+Bistro+KW+{0:00}.pdf";
 
         /// <summary>
+        /// Path of the configuration file.
+        /// </summary>
+        static string ConfigFile = @"E:\Visual Studio\Projects\MensaScanner\accesscodes.txt";
+
+        /// <summary>
         /// Culture data for german strings and calender.
         /// </summary>
         static CultureInfo CultureGerman = new CultureInfo("de-DE");
 
+        /// <summary>
+        /// Program entry point.
+        /// </summary>
+        /// <param name="args">Unused.</param>
         static void Main(string[] args)
         {
-            for(int d = 11; d <= 14; ++d)
+            // Load config file
+            var config = ReadConfig();
+
+            // Need to get authentication code?
+            if(!config.ContainsKey("AuthCode"))
             {
-                Console.WriteLine("\n\n\n######### Day " + d + " ##########");
-
-                Console.WriteLine();
-                Console.WriteLine("MENSA:");
-                var mensaMenu = ParseMensa(new DateTime(2018, 10, d));
-                foreach(var menuEntry in mensaMenu)
-                    Console.WriteLine(menuEntry.Name + "    ||    " + menuEntry.Price);
-
-                Console.WriteLine();
-                Console.WriteLine("UKSH:");
-                var uhshMenu = ParseUksh(new DateTime(2018, 10, d));
-                foreach(var menuEntry in uhshMenu)
-                    Console.WriteLine(menuEntry.Name + "    ||    " + menuEntry.Price);
+                // Ask user for code
+                Console.WriteLine("Please open the following URL and copy the final \"code\" parameter:");
+                Console.WriteLine(config["URL"]);
+                Console.Write("Code: ");
+                string code = Console.ReadLine();
+                config["AuthCode"] = code;
+                SaveConfig(config);
+                Console.WriteLine("Authentication code saved.");
             }
+            // Protocol on https://developer.webex.com/authentication.html
+            if(!config.ContainsKey("AccessToken"))
+            {
+                // Use authentication code to gain access token
+                Console.WriteLine("Requesting first access token...");
+                JObject response;
+                IEnumerable<KeyValuePair<string, string>> requestParams = new List<KeyValuePair<string, string>>
+                {
+                       new KeyValuePair<string, string>("grant_type", "authorization_code"),
+                       new KeyValuePair<string, string>("client_id", config["ClientID"]),
+                       new KeyValuePair<string, string>("client_secret", config["ClientSecret"]),
+                       new KeyValuePair<string, string>("code", config["AuthCode"]),
+                       new KeyValuePair<string, string>("redirect_uri", "http://invalid/"),
+                };
+                using(var httpClient = new HttpClient())
+                using(var htmlForm = new FormUrlEncodedContent(requestParams))
+                {
+                    htmlForm.Headers.Clear();
+                    htmlForm.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+                    string responseContentRaw = httpClient.PostAsync("https://api.ciscospark.com/v1/access_token", htmlForm).Result.Content.ReadAsStringAsync().Result;
+                    response = JObject.Parse(responseContentRaw);
+                }
+
+                // Extract access token
+                config["AccessToken"] = (string)response["access_token"];
+                config["AccessTokenExpires"] = DateTime.Now.AddSeconds(double.Parse((string)response["expires_in"])).ToFileTimeUtc().ToString();
+                config["RefreshToken"] = (string)response["refresh_token"];
+                config["RefreshTokenExpires"] = DateTime.Now.AddSeconds(double.Parse((string)response["refresh_token_expires_in"])).ToFileTimeUtc().ToString();
+                SaveConfig(config);
+                Console.WriteLine("Requesting first access token successful.");
+            }
+            // TODO refresh token
+
+            // Retrieve menus
+            Console.WriteLine("Retrieving menus...");
+            Console.WriteLine();
+            Console.WriteLine("MENSA:");
+            var mensaMenu = ParseMensa(DateTime.Today);
+            foreach(var menuEntry in mensaMenu)
+                Console.WriteLine($"- {menuEntry.Name}    [{menuEntry.Price}]");
+
+            Console.WriteLine();
+            Console.WriteLine("UKSH:");
+            var ukshMenu = ParseUksh(DateTime.Today);
+            foreach(var menuEntry in ukshMenu)
+                Console.WriteLine($"- {menuEntry.Name}    [{menuEntry.Price}]");
+
+            // Prepare message markdown
+            Console.WriteLine("Preparing message...");
+            StringBuilder message = new StringBuilder();
+            message.AppendLine("**Mensa**");
+            foreach(var menuEntry in mensaMenu)
+                message.AppendLine($"- {menuEntry.Name}    [{menuEntry.Price}]");
+            message.AppendLine();
+            message.AppendLine("**UKSH Bistro**");
+            foreach(var menuEntry in ukshMenu)
+                message.AppendLine($"- {menuEntry.Name}    [{menuEntry.Price}]");
+
+            // Send message
+            Console.WriteLine("Posting message...");
+            HttpWebRequest messagePostRequest = WebRequest.CreateHttp("https://api.ciscospark.com/v1/messages");
+            {
+                // Set HTTP headers
+                messagePostRequest.Method = "POST";
+                messagePostRequest.Headers.Clear();
+                messagePostRequest.Headers.Add("Content-type: application/json; charset=utf-8");
+                messagePostRequest.Headers.Add("Authorization: Bearer " + config["AccessToken"]);
+
+                // Put message data into JSON object
+                JObject messageData = new JObject
+                {
+                    { "roomId", config["RoomID"] },
+                    { "markdown", message.ToString() }
+                };
+
+                // Set message as HTTP request content
+                using(StreamWriter w = new StreamWriter(messagePostRequest.GetRequestStream()))
+                    w.Write(messageData.ToString());
+
+                // Get response
+                using(HttpWebResponse messagePostResponse = (HttpWebResponse)messagePostRequest.GetResponse())
+                    if(messagePostResponse.StatusCode == HttpStatusCode.OK)
+                        Console.WriteLine("Posting message successful.");
+                    else
+                        Console.WriteLine("Posting message failed: " + (int)messagePostResponse.StatusCode);
+            }
+
             Console.ReadLine();
         }
 
@@ -189,12 +287,36 @@ namespace MensaScanner
 
                 // Extract menu entry name
                 string menuEntryName = Regex.Match(dayMenuRowColumns[0].Groups[1].Value, "<strong>(.*?)</strong>", RegexOptions.IgnoreCase | RegexOptions.Singleline).Groups[1].Value.Trim();
+                menuEntryName = Regex.Replace(menuEntryName, "(<small>.*?</small>)|(<br ?/?>)", "", RegexOptions.IgnoreCase);
 
                 // Extract menu entry price
                 string menuEntryPrice = dayMenuRowColumns[2].Groups[1].Value.Trim();
 
                 // Add menu entry to returned list
                 yield return new MenuEntry { Name = menuEntryName, Price = menuEntryPrice };
+            }
+        }
+
+        static Dictionary<string, string> ReadConfig()
+        {
+            // Read config file
+            Dictionary<string, string> config = new Dictionary<string, string>();
+            foreach(string line in File.ReadAllLines(ConfigFile))
+            {
+                // Split key and value
+                int splitIndex = line.IndexOf(':');
+                config.Add(line.Substring(0, splitIndex), line.Substring(splitIndex + 1));
+            }
+            return config;
+        }
+
+        static void SaveConfig(Dictionary<string, string> config)
+        {
+            // Write config file
+            using(StreamWriter w = new StreamWriter(File.Open(ConfigFile, FileMode.Create)))
+            {
+                foreach(var entry in config)
+                    w.WriteLine(entry.Key + ":" + entry.Value);
             }
         }
     }
